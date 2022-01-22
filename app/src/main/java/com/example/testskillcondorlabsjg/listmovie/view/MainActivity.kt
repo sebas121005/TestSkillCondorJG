3e014179d0d87dcf10847a54126b66de4b708d71
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 16:52:50 -0500

    Se mueve clase de MainActivity al paquete de view de listmovie y se agrega el adaptador para mostrar la lista de las peliculas

--- app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MainActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testskillcondorlabsjg
+package com.example.testskillcondorlabsjg.listmovie.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testskillcondorlabsjg.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
