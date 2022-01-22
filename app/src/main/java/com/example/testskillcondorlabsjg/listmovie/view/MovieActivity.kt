8b45c35e0fc152f7a79e480c950afe5cddd90445
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 18:21:00 -0500

    Se agrega variable del adaptador de MovieListAdapter y se inicializa el adapatador y el recylcler view para mostrar la data de las peliculas

--- app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MovieActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MovieActivity.kt
@@ -2,20 +2,29 @@ package com.example.testskillcondorlabsjg.listmovie.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testskillcondorlabsjg.R
 import com.example.testskillcondorlabsjg.databinding.ActivityMainBinding
+import com.example.testskillcondorlabsjg.listmovie.view.adapter.MovieListAdapter
 
 class MovieActivity : AppCompatActivity() {
-    var movieBinding: ActivityMainBinding? = null
-    
+    private var movieBinding: ActivityMainBinding? = null
+    private var movieListAdapter: MovieListAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         movieBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = movieBinding?.root
         setContentView(view)
+        initializeWidgets()
     }
 
     fun initializeWidgets() {
+        movieListAdapter = MovieListAdapter(this, ArrayList())
 
+        with(movieBinding?.listMovies) {
+            this?.layoutManager = LinearLayoutManager(this@MovieActivity)
+            this?.adapter = movieListAdapter
+        }
     }
 }
