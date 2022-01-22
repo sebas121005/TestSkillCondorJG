1b02c4c32b6bc656f4cdd7de0b2cd50768fc7207
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 17:38:44 -0500

    Se agrega variable de la url de la imagen para utilizarla a nivel general del proyecto en el archivo de gradle.properties y se vuelven privadas las variables sugeridas por el IDE

--- app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/adapter/MovieListAdapter.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/adapter/MovieListAdapter.kt
@@ -10,10 +10,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.testskillcondorlabsjg.R
 import com.example.testskillcondorlabsjg.listmovie.model.Movie
 
-class MovieListAdapter(private val context: Context, val dataMovie: List<Movie>): RecyclerView.Adapter<MovieListAdapter.ViewHolderMovie>() {
+class MovieListAdapter(private val context: Context, private val dataMovie: List<Movie>):
+    RecyclerView.Adapter<MovieListAdapter.ViewHolderMovie>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderMovie {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.item_movie, parent,
+            false)
 
         return ViewHolderMovie(view)
 
@@ -30,7 +32,7 @@ class MovieListAdapter(private val context: Context, val dataMovie: List<Movie>)
     class ViewHolderMovie(itemView: View): RecyclerView.ViewHolder(itemView) {
         private var mPosterImageview: ImageView? = null
         private var mNameMovieTextView: TextView? = null
-        var mAverageTextView: TextView? = null
+        private var mAverageTextView: TextView? = null
         init {
             mPosterImageview = itemView.findViewById(R.id.poster_image)
             mNameMovieTextView = itemView.findViewById(R.id.movie_name)
