53759d9e73f4b4f8242e846a589de7f9e25d06df
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 18:15:13 -0500

    Se agrega uso de viewBinding para la vista de MoviewActivity

--- app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MovieActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MovieActivity.kt
@@ -3,10 +3,19 @@ package com.example.testskillcondorlabsjg.listmovie.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.testskillcondorlabsjg.R
+import com.example.testskillcondorlabsjg.databinding.ActivityMainBinding
 
 class MovieActivity : AppCompatActivity() {
+    var movieBinding: ActivityMainBinding? = null
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        movieBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = movieBinding?.root
+        setContentView(view)
+    }
+
+    fun initializeWidgets() {
+
     }
 }
