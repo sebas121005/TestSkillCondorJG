94477c81909d80a805f3312807726853099913bd
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 18:45:40 -0500

    Se inicializa variable de okHttpclient y gson y tambén se agrega clase de HttpLogginInterceptor para el interceptor del okhttoclient

--- app/src/main/java/com/example/testskillcondorlabsjg/api/RetrofitManager.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/api/RetrofitManager.kt
@@ -1,5 +1,16 @@
 package com.example.testskillcondorlabsjg.api
 
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
 class RetrofitManager {
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
+    private val gson = GsonBuilder()
+        .setLenient()
+        .create()
 
 }
