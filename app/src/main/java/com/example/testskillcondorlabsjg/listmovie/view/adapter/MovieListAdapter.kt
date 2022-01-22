36284354d74d2a48463b3a40415d31035cb70d90
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 18:03:54 -0500

    Se agrega uso de viewBinding en la clase MovieListAdapter y se modifica el uso de findViewById para obtener los componenes directamente y mostrar su respectivo valor

--- app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/adapter/MovieListAdapter.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/adapter/MovieListAdapter.kt
@@ -2,22 +2,19 @@ package com.example.testskillcondorlabsjg.listmovie.view.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.testskillcondorlabsjg.R
+import com.example.testskillcondorlabsjg.databinding.ItemMovieBinding
 import com.example.testskillcondorlabsjg.listmovie.model.Movie
 
 class MovieListAdapter(private val context: Context, private val dataMovie: List<Movie>):
     RecyclerView.Adapter<MovieListAdapter.ViewHolderMovie>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderMovie {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.item_movie, parent,
-            false)
+        val itemMovieBinding = ItemMovieBinding.inflate(LayoutInflater.from(context),
+            parent, false)
 
-        return ViewHolderMovie(view)
+        return ViewHolderMovie(itemMovieBinding)
 
     }
 
@@ -29,19 +26,12 @@ class MovieListAdapter(private val context: Context, private val dataMovie: List
         return dataMovie.size
     }
 
-    class ViewHolderMovie(itemView: View): RecyclerView.ViewHolder(itemView) {
-        private var mPosterImageview: ImageView? = null
-        private var mNameMovieTextView: TextView? = null
-        private var mAverageTextView: TextView? = null
-        init {
-            mPosterImageview = itemView.findViewById(R.id.poster_image)
-            mNameMovieTextView = itemView.findViewById(R.id.movie_name)
-            mAverageTextView = itemView.findViewById(R.id.average_movie)
-        }
+    class ViewHolderMovie(private val itemMovieBinding: ItemMovieBinding):
+            RecyclerView.ViewHolder(itemMovieBinding.root) {
 
         fun addItemMovie(movie: Movie) {
-            mNameMovieTextView?.text = movie.movieName
-            mAverageTextView?.text = movie.movieAverage.toString()
+            itemMovieBinding.movieName.text = movie.movieName
+            itemMovieBinding.averageMovie.text = movie.movieAverage.toString()
         }
     }
 }
