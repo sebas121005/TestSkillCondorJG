c49e720a6701206e888e5ffe6ebab976b8a6a1cf
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 18:47:49 -0500

    Se agrega función de createWebService que contiene la incialización del builder de retrofit

--- app/src/main/java/com/example/testskillcondorlabsjg/api/RetrofitManager.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/api/RetrofitManager.kt
@@ -2,6 +2,8 @@ package com.example.testskillcondorlabsjg.api
 
 import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class RetrofitManager {
@@ -12,5 +14,14 @@ class RetrofitManager {
     private val gson = GsonBuilder()
         .setLenient()
         .create()
+
+    fun createWebService(url: String): WebService {
+        val builder = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+
+        return builder.build().create(WebService::class.java)
+    }
 
 }
