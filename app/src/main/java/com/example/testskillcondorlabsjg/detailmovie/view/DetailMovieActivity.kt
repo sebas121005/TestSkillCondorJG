700df121e758a445d23468caf91e37f9ca464b4e
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 21:50:45 -0500

    Se agrega activity para mostrar el detalle de la pelicula y se agrega viewBinding al layout de activity_detail_movie

---
+++ app/src/main/java/com/example/testskillcondorlabsjg/detailmovie/view/DetailMovieActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testskillcondorlabsjg.detailmovie.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testskillcondorlabsjg.R
+
+class DetailMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_movie)
+    }
+}
