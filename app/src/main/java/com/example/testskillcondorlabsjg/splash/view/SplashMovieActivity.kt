1c4391e9a72c391a11d40f7ac4dbfc581190cfb9
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-23 15:35:31 -0500

    Se modifica diseño de imagen del splash y se se agrega clase SystemClock para el splash

--- app/src/main/java/com/example/testskillcondorlabsjg/splash/view/SplashMovieActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/splash/view/SplashMovieActivity.kt
@@ -1,13 +1,11 @@
 package com.example.testskillcondorlabsjg.splash.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
-import com.example.testskillcondorlabsjg.R
+import android.os.SystemClock
+import androidx.appcompat.app.AppCompatActivity
 import com.example.testskillcondorlabsjg.databinding.ActivitySplashMovieBinding
 import com.example.testskillcondorlabsjg.listmovie.view.MovieActivity
-import com.example.testskillcondorlabsjg.listmovie.viewmodel.MovieListViewModel
 
 class SplashMovieActivity : AppCompatActivity() {
     private var mViewBinding: ActivitySplashMovieBinding? = null
@@ -18,7 +16,7 @@ class SplashMovieActivity : AppCompatActivity() {
         val view = mViewBinding?.root
         setContentView(view)
 
-        Thread.sleep(2000)
+        SystemClock.sleep(2000);
 
         startActivity(Intent(this, MovieActivity::class.java))
 
