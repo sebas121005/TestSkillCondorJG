e460632f614517c0a34c550c72ec24c28dada6c8
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 19:19:39 -0500

    Se agrega corrutina para realizar el proceso de consumir el endpoint que obtiene las peliculas y se agrega lista mutable para poder almacenar la información

--- app/src/main/java/com/example/testskillcondorlabsjg/listmovie/viewmodel/MovieListViewModel.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/listmovie/viewmodel/MovieListViewModel.kt
@@ -1,11 +1,33 @@
 package com.example.testskillcondorlabsjg.listmovie.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.testskillcondorlabsjg.MovieRepository
+import com.example.testskillcondorlabsjg.api.ApiCallbackHelper
+import com.example.testskillcondorlabsjg.listmovie.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MovieListViewModel: ViewModel() {
     private val movieRepository = MovieRepository()
+    val showListMoviesLiveDate = MutableLiveData<Movie>()
 
     fun getMovies() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                movieRepository.getMovies(object : ApiCallbackHelper<Movie> {
+                    override fun onSuccess(response: Movie) {
+                        showListMoviesLiveDate.value = response
+                    }
+
+                    override fun onFailure() {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+            }
+        }
     }
 }
