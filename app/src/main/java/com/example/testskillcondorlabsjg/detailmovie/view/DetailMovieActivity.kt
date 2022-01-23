f5a8183c2be87370978966f853f1ae54af3095f5
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-23 01:31:44 -0500

    Se agrega función de initializeWidget y se configura la toolbar con su respectivo evento onclick para quitar la actividad y volver a la anterior en el momento que se pulse la fecha atrás

--- app/src/main/java/com/example/testskillcondorlabsjg/detailmovie/view/DetailMovieActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/detailmovie/view/DetailMovieActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import com.example.testskillcondorlabsjg.R
 import com.example.testskillcondorlabsjg.databinding.ActivityDetailMovieBinding
 import com.example.testskillcondorlabsjg.listmovie.viewmodel.MovieListViewModel
+import android.view.MenuItem
+
+
+
 
 class DetailMovieActivity : AppCompatActivity() {
     private var mDetailMovieBinding: ActivityDetailMovieBinding? = null
@@ -15,6 +19,13 @@ class DetailMovieActivity : AppCompatActivity() {
         val view = mDetailMovieBinding?.root
         setContentView(view)
 
+        initializeWidget()
+    }
+
+    private fun initializeWidget() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Detalle Pelicula"
+
         showDetailMovie()
     }
 
@@ -24,5 +35,15 @@ class DetailMovieActivity : AppCompatActivity() {
         mDetailMovieBinding?.budgetMovie?.text = intent.extras?.getString(MovieListViewModel.BUDGET_MOVIE)
         mDetailMovieBinding?.budgetMovie?.text = intent.extras?.getString(MovieListViewModel.BUDGET_MOVIE)
         intent.extras?.getBoolean(MovieListViewModel.TRAILER_MOVIE)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
