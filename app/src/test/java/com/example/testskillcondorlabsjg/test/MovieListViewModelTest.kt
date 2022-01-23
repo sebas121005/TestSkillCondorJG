a8c5048aaa8ec2bfbecd309005f4863b6ea6113e
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-23 15:08:34 -0500

    Se agrega archivo de pruebas unitarias

---
+++ app/src/test/java/com/example/testskillcondorlabsjg/test/MovieListViewModelTest.kt
@@ -0,0 +1,20 @@
+package com.example.testskillcondorlabsjg.test
+
+import com.example.testskillcondorlabsjg.listmovie.model.Movie
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class MovieListViewModelTest {
+
+    @Before
+    fun setUp() {
+
+    }
+
+    @Test
+    fun getMovies() {
+        
+    }
+}
