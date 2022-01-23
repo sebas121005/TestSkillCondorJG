eff056b72ea16ffeed73dca50d9dbcde10fb4c8d
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-23 15:21:19 -0500

    Se implementa funcionalidad de viewModel y viewBinding para la vista del splash

--- app/src/main/java/com/example/testskillcondorlabsjg/splash/view/SplashMovieActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/splash/view/SplashMovieActivity.kt
@@ -2,11 +2,21 @@ package com.example.testskillcondorlabsjg.splash.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.testskillcondorlabsjg.R
+import com.example.testskillcondorlabsjg.databinding.ActivitySplashMovieBinding
+import com.example.testskillcondorlabsjg.listmovie.viewmodel.MovieListViewModel
 
 class SplashMovieActivity : AppCompatActivity() {
+    private var mMovieViewModel: MovieListViewModel? = null
+    private var mViewBinding: ActivitySplashMovieBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_movie)
+        mViewBinding = ActivitySplashMovieBinding.inflate(layoutInflater)
+        val view = mViewBinding?.root
+        setContentView(view)
+
+        mMovieViewModel = ViewModelProvider(this)[MovieListViewModel::class.java]
     }
 }
