f70cb1f46ac07c5b70ea8fdc54265cdb9c3c7e7d
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-23 00:28:54 -0500

    Se agrega toggleButton para determinar si es una pelicula favorita y se iniciailiza viewBinding para la vista de DetailMovieActivity

--- app/src/main/java/com/example/testskillcondorlabsjg/detailmovie/view/DetailMovieActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/detailmovie/view/DetailMovieActivity.kt
@@ -3,10 +3,15 @@ package com.example.testskillcondorlabsjg.detailmovie.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.testskillcondorlabsjg.R
+import com.example.testskillcondorlabsjg.databinding.ActivityDetailMovieBinding
 
 class DetailMovieActivity : AppCompatActivity() {
+    private var mDetailMovieBinding: ActivityDetailMovieBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_movie)
+
+        mDetailMovieBinding = ActivityDetailMovieBinding.inflate(layoutInflater)
+        val view = mDetailMovieBinding?.root
+        setContentView(view)
     }
 }
