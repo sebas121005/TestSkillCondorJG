225a31837698d39506752ae2f523705ec9fb31da
Juan Grisales <jgrisales@ikatechsolutions.com>
2022-01-22 19:52:51 -0500

    Se inicializar viewModel en la clase MovieActivity y se agrega observador de la lista mutable para notificar en el momento de que se obtengan los datos se puedan mostrar en el adaptador del recyclerview

--- app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MovieActivity.kt
+++ app/src/main/java/com/example/testskillcondorlabsjg/listmovie/view/MovieActivity.kt
@@ -2,29 +2,45 @@ package com.example.testskillcondorlabsjg.listmovie.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.testskillcondorlabsjg.R
 import com.example.testskillcondorlabsjg.databinding.ActivityMainBinding
+import com.example.testskillcondorlabsjg.listmovie.model.Movie
 import com.example.testskillcondorlabsjg.listmovie.view.adapter.MovieListAdapter
+import com.example.testskillcondorlabsjg.listmovie.viewmodel.MovieListViewModel
 
 class MovieActivity : AppCompatActivity() {
-    private var movieBinding: ActivityMainBinding? = null
-    private var movieListAdapter: MovieListAdapter? = null
+    private var mMovieBinding: ActivityMainBinding? = null
+    private var mMovieListAdapter: MovieListAdapter? = null
+    private var mMovieViewModel: MovieListViewModel? = null
+
+    private var dataMovieList = ArrayList<Movie>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        movieBinding = ActivityMainBinding.inflate(layoutInflater)
-        val view = movieBinding?.root
+        mMovieBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mMovieBinding?.root
         setContentView(view)
         initializeWidgets()
+        observables()
     }
 
     private fun initializeWidgets() {
-        movieListAdapter = MovieListAdapter(this, ArrayList())
-
-        with(movieBinding?.listMovies) {
+        mMovieViewModel = ViewModelProvider(this)[MovieListViewModel::class.java]
+        mMovieListAdapter = MovieListAdapter(this, dataMovieList)
+        with(mMovieBinding?.listMovies) {
             this?.layoutManager = LinearLayoutManager(this@MovieActivity)
-            this?.adapter = movieListAdapter
+            this?.adapter = mMovieListAdapter
         }
+    }
+
+    private fun observables() {
+        mMovieViewModel?.showListMoviesLiveDate?.observe(this, {
+            it.resultsMovie?.let { listMovies ->
+                dataMovieList.clear()
+                dataMovieList.addAll(listMovies)
+                mMovieListAdapter?.notifyDataSetChanged()
+            }
+        })
     }
 }
